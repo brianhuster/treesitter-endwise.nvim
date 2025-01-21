1eb6f54a67c75380f525a1f46e1def4fc00433a1
brianhuster <phambinhanctb2004@gmail.com>
2025-01-22 02:36:29 +0700

    fix: support Query:iter_matches changes in upstream neovim
    Copied from https://github.com/RRethy/nvim-treesitter-endwise/pull/40

--- lua/nvim-treesitter/endwise.lua
+++ lua/nvim-treesitter/endwise.lua
@@ -192,6 +192,9 @@ local function endwise(bufnr)
     for _, match, metadata in query:iter_matches(root, bufnr, range[1], range[3] + 1, query_opts) do
         local indent_node, cursor_node, endable_node
         for id, node in pairs(match) do
+            if vim.fn.has('nvim-0.11') == 1 then
+                node = node[#node]
+            end
             if query.captures[id] == 'indent' then
                 indent_node = node
             elseif query.captures[id] == 'cursor' then
